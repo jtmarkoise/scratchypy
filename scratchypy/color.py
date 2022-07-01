6234df76a59f2387d38292e4765a7a4bab18d3f4
tcool <tcool@nowhere.ninja>
2022-07-01 14:17:28 -0500

    fixed and added colors.

--- scratchypy/color.py
+++ scratchypy/color.py
@@ -7,6 +7,9 @@ Created on Apr 1, 2022
 import pygame
 BLACK = pygame.Color(0,0,0)
 WHITE = pygame.Color(0xff,0xff,0xff)
-GREEN = pygame.Color(0,255,255)
+GREEN = pygame.Color(0,255,0)
 BLUE = pygame.Color(0,0,255)
 RED = pygame.Color(255,0,0)
+YELLOW = pygame.Color(255,255,0)
+ORANGE = pygame.Color(255,128,0)
+PURPLE = pygame.Color(255,0,255)
